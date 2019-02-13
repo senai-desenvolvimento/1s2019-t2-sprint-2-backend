ac263c7f59fe9baa246cfcde27b7e3bcbfadf08f
corujasdevbr <fernando.guerra@corujasdev.com.br>
2019-02-13 19:16:07 -0200

    Svigufo_Manha

---
+++ manha/Senai.SviGufo.WebApi/Senai.SviGufo.WebApi/Domains/TipoEventoDomain.cs
@@ -0,0 +1,11 @@
+﻿namespace Senai.SviGufo.WebApi.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade Tipo Evento
+    /// </summary>
+    public class TipoEventoDomain
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
